16677559637161de2f14c3f0eb4342bdd377340d
Mat Sz <contact@matsz.dev>
2020-04-04 18:57:29 +0200

    added a test

---
+++ __tests__/connection.test.ts
@@ -0,0 +1,34 @@
+import { microMTAConnection } from '../src/connection';
+
+class Socket {
+  lastMessage = '';
+
+  setEncoding(encoding: string) {}
+
+  on(event: string, listener: Function) {}
+
+  write(data: string) {
+    const messages = data.split('\r\n');
+    if (messages[messages.length - 1] === '') {
+      this.lastMessage = messages[messages.length - 2];
+    } else {
+      this.lastMessage = messages[messages.length - 1];
+    }
+  }
+
+  destroy() {}
+}
+
+describe('connection', () => {
+  it('welcomes clients', () => {
+    const socket = new Socket();
+    const connection = new microMTAConnection(
+      socket as any,
+      { hostname: 'localhost', size: 100000 },
+      () => {},
+      () => {}
+    );
+
+    expect(socket.lastMessage).toEqual('220 localhost ESMTP microMTA');
+  });
+});
