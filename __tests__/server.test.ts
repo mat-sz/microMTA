b7e191448231eadc00b0877dbfc36be7ff2ac32e
Mat Sz <contact@matsz.dev>
2020-08-14 18:05:57 +0200

    added server tests

---
+++ __tests__/server.test.ts
@@ -0,0 +1,46 @@
+import { microMTA } from '../src';
+import { Socket } from 'net';
+
+let currentPort = 2525;
+const nextPort = () => {
+  currentPort++;
+  return currentPort;
+};
+
+describe('server', () => {
+  it('starts listening without exceptions', () => {
+    const port = nextPort();
+    const mta = new microMTA({
+      hostname: 'localhost',
+      ip: '127.0.0.1',
+      port,
+    });
+    mta.close();
+  });
+
+  it('closes client socket on .close', done => {
+    const port = nextPort();
+    const mta = new microMTA({
+      hostname: 'localhost',
+      ip: '127.0.0.1',
+      port,
+    });
+
+    const socket = new Socket();
+    const onData = jest.fn();
+    socket.on('connect', () => {
+      socket.write('EHLO\r\n');
+    });
+    socket.on('data', data => {
+      onData(data.toString());
+      mta.close();
+    });
+    socket.on('close', () => {
+      expect(onData).toHaveBeenCalledWith(
+        '421 The server is shutting down.\r\n'
+      );
+      done();
+    });
+    socket.connect({ host: '127.0.0.1', port });
+  });
+});
