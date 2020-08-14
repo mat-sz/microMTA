e806ac0ab68e321d2464ff4ec9672a6855f67745
Mat Sz <contact@matsz.dev>
2020-08-14 18:12:55 +0200

    updated comments for replies

--- src/smtp/replies.ts
+++ src/smtp/replies.ts
@@ -1,5 +1,27 @@
 type SMTPReply = [number, string];
 
+// 2xx Success
+export const BYE: SMTPReply = [221, 'Bye'];
+export const AUTHENTICATION_SUCCESSFUL: SMTPReply = [
+  235,
+  'Authentication successful',
+];
+export const OK: SMTPReply = [250, 'Ok'];
+export const TLS_GO_AHEAD: SMTPReply = [220, 'TLS go ahead'];
+
+// 3xx Additional data required
+export const START_MAIL_INPUT: SMTPReply = [
+  354,
+  'End data with <CR><LF>.<CR><LF>',
+];
+
+// 4xx Persistent transient failure
+export const SERVER_SHUTTING_DOWN: SMTPReply = [
+  421,
+  'The server is shutting down',
+];
+
+// 5xx Permanent errors
 export const SYNTAX_ERROR: SMTPReply = [500, 'Syntax error'];
 export const SYNTAX_ERROR_ARGUMENT: SMTPReply = [
   501,
@@ -16,21 +38,3 @@ export const BAD_USERNAME_OR_PASSWORD: SMTPReply = [
   'Bad username or password',
 ];
 export const MAXIMUM_SIZE_EXCEEDED: SMTPReply = [552, 'Maximum size exceeded'];
-
-export const BYE: SMTPReply = [221, 'Bye'];
-export const AUTHENTICATION_SUCCESSFUL: SMTPReply = [
-  235,
-  'Authentication successful',
-];
-export const OK: SMTPReply = [250, 'Ok'];
-export const TLS_GO_AHEAD: SMTPReply = [220, 'TLS go ahead'];
-
-export const START_MAIL_INPUT: SMTPReply = [
-  354,
-  'End data with <CR><LF>.<CR><LF>',
-];
-
-export const SERVER_SHUTTING_DOWN: SMTPReply = [
-  421,
-  'The server is shutting down',
-];
